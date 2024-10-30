3a3a5846f8f5d006c1fc1354a1c31de6176f4a30
jobet1995 <jobetcasquejo221@gmail.com>
2024-10-30 19:40:41 +0800

    added new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `My_Expense`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap","gatsby-plugin-styled-components", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
